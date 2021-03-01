de7376f47b2f168d1c4b781b09e08846928a99f2
lr250195 <lr250195@ncr.com>
2021-03-01 09:58:30 -0300

    [feat] - Mensajes custom

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -2,15 +2,15 @@
 
 def call(String buildResult) {
     if ( buildResult == "SUCCESS" ) {
-        slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+        slackSend color: "good", message: "${env.JOB_NAME} - Build number ${env.BUILD_NUMBER}\nExecution status SUCCESSFULL - For more info go to ${env.BUILD_URL}"
     }
     else if( buildResult == "FAILURE" ) { 
-        slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+        slackSend color: "danger", message: "${env.JOB_NAME} - Build number ${env.BUILD_NUMBER}\nExecution status FAILED - For more info go to ${env.BUILD_URL}"
     }
     else if( buildResult == "UNSTABLE" ) { 
-        slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+        slackSend color: "warning", message: "${env.JOB_NAME} - Build number ${env.BUILD_NUMBER}\nExecution status UNSTABLE - For more info go to ${env.BUILD_URL}"
     }
     else {
-        slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+        slackSend color: "danger", message: "${env.JOB_NAME} - Build number ${env.BUILD_NUMBER}\nExecution status UNCLEAR - For more info go to ${env.BUILD_URL}"
     }
 }
